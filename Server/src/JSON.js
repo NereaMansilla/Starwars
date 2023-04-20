9ccb9a633d8e1f1171db36ed2c96fc632c2b05bf
Nerea <NereaMansillaBarreiro@gmail.com>
2023-04-20 02:23:57 -0300

    Changes movies JSON description

--- Server/src/JSON.js
+++ Server/src/JSON.js
@@ -1,67 +1,67 @@
 export const Movies = [
     {
         id: 1,
-        title: "Star wars episodio IV: Una nueva esperanza", 
+        title: "Star wars episode IV: A new hope", 
         image: "https://th.bing.com/th/id/R.1cf1602fd9a8cf19ee669ac28bbe4d27?rik=Q%2bW79XVeU5mQjw&pid=ImgRaw&r=0",
-        description: "La princesa Leia, líder del movimiento rebelde que desea reinstaurar la República en la galaxia en los tiempos ominosos del Imperio, es capturada por las Fuerzas Imperiales, capitaneadas por el implacable Darth Vader, el sirviente más fiel del Emperador. El intrépido y joven Luke Skywalker, ayudado por Han Solo, capitán de la nave espacial El Halcón Milenario, y los androides, R2D2 y C3PO, serán los encargados de luchar contra el enemigo e intentar rescatar a la princesa para volver a instaurar la justicia en el seno de la galaxia.",
+        description: "Luke Skywalker begins a journey that will change the galaxy in Star Wars: Episode IV - A New Hope. Nineteen years after the formation of the Empire, Luke is thrust into the struggle of the Rebel Alliance when he meets Obi-Wan Kenobi, who has lived for years in seclusion on the desert planet of Tatooine. Obi-Wan begins Luke’s Jedi training as Luke joins him on a daring mission to rescue the beautiful Rebel leader Princess Leia from the clutches of Darth Vader and the evil Empire.",
         actors: "Mark Hamill, Harrison Ford, Carrie Fisher, Alec Guinness, Peter Cushing, David Prowse, Peter Mayhew, Anthony Daniels, Kenny Baker, Phil Brown, Shelagh Fraser, Garrick Hagon, Denis Lawson, Alex McCrindle, Richard LeParmentier, Drewe Henley, Jack Purvis, Don Henderson, William Hootkins, Malcolm Tierney"
 
     },
     {
         id: 2,
-        title: "Star wars episodio V: El imperio contrataca",
+        title: "Star wars episode V: The empire strikes back",
         image: "https://th.bing.com/th/id/R.e084bfbcf6357f8d2f1c2678ac505401?rik=XEOiY6XL5%2feqPw&riu=http%3a%2f%2fboomstickcomics.com%2fwp-content%2fuploads%2f2015%2f04%2fstar-wars-episode-v-the-empire-strikes-back-5229c2d4a1c75.jpg&ehk=wkfMaMDAN7Rt5Tv20KT7P7t0joXoCmelEoNgNwjgjCg%3d&risl=&pid=ImgRaw&r=0",
-        description: "Tras un ataque sorpresa de las tropas imperiales a las bases camufladas de la alianza rebelde, Luke Skywalker, en compañía de R2D2, parte hacia el planeta Dagobah en busca de Yoda, el último maestro Jedi, para que le enseñe los secretos de la Fuerza. Mientras, Han Solo, la princesa Leia, Chewbacca, y C3PO esquivan a las fuerzas imperiales y piden refugio al antiguo propietario del Halcón Milenario, Lando Calrissian, en la ciudad minera de Bespin, donde les prepara una trampa urdida por Darth Vader",
+        description: "After the destruction of the Death Star, Imperial forces continue to pursue the Rebels. After the Rebellion’s defeat on the ice planet Hoth, Luke journeys to the planet Dagobah to train with Jedi Master Yoda, who has lived in hiding since the fall of the Republic. In an attempt to convert Luke to the dark side, Darth Vader lures young Skywalker into a trap in the Cloud City of Bespin.",
         actors:"Mark Hamill, Harrison Ford, Carrie Fisher, Frank Oz, Billy Dee Williams, David Prowse, Alec Guinness, Anthony Daniels, Kenny Baker, Peter Mayhew, Jeremy Bulloch, Kenneth Colley, Bruce Boa, Julian Glover, Denis Lawson, Michael Culver, John Ratzenberger, Michael Sheard"
     },
     {
      id:3,
-     title: "Star wars episodio VI: El retorno del Jedi",
+     title: "Star wars episode VI: Return of the Jedi",
      image: "https://th.bing.com/th/id/R.f7df35e1eaffb6fccd2b7a4378765277?rik=qrO1ooKpjeqX5w&pid=ImgRaw&r=0",
-     description: "Para ir a Tatooine y liberar a Han Solo, Luke Skywalker y la princesa Leia deben infiltrarse en la peligrosa guarida de Jabba the Hutt, el gángster más temido de la galaxia. Una vez reunidos, el equipo recluta a tribus de Ewoks para combatir a las fuerzas imperiales en los bosques de la luna de Endor. Mientras tanto, el Emperador y Darth Vader conspiran para atraer a Luke al lado oscuro, pero el joven está decidido a reavivar el espíritu del Jedi en su padre. La guerra civil galáctica termina con un último enfrentamiento entre las fuerzas rebeldes unificadas y una segunda Estrella de la Muerte, indefensa e incompleta, en una batalla que decidirá el destino de la galaxia",
+     description: "After a quick trip back to Tatooine, Luke Skywalker, Leia Organa, and Han Solo are reunited and join up with the amassing rebel fleet to take down the evil Empire once and for all. But the Empire is plotting too. Emperor Palpatine commands his troops aboard his newly consturcted Death Star stationed above the forest moon of Endor, where the rebels - and some unlikely furry friends - make their stand against the Empire. While Luke Skywalker confronts Darth Vader on the Death Star once more, Han leads a team to take out a shield protecting the battle station so that the rebel fleet can destory it once more and finally put an end to the war...",
      actors: "Mark Hamill, Harrison Ford, Carrie Fisher, David Prowse, Ian McDiarmid, Billy Dee Williams, Frank Oz, Alec Guinness, Anthony Daniels, Kenny Baker, Peter Mayhew, Sebastian Shaw, Warwick Davis, Caroline Blakiston, Dermot Crowley, Kenneth Colley, Denis Lawson, Michael Pennington"
     },
     {
         id:4,
-        title: "Star wars episodio I: La amenaza fantasma",
+        title: "Star wars episode I: The panthom menace",
         image:"https://th.bing.com/th/id/R.d0344fe39ab28eba9c87113ee5a75693?rik=aKmoS9H4nngaAQ&pid=ImgRaw&r=0",
-        description: "Ambientada treinta años antes que La guerra de las galaxias (1977), muestra la infancia de Darth Vader, el pasado de Obi-Wan Kenobi y el resurgimiento de los Sith, los caballeros Jedi dominados por el Lado Oscuro. La Federación de Comercio ha bloqueado el pequeño planeta de Naboo, gobernado por la joven Reina Amidala; se trata de un plan ideado por Sith Darth Sidious, que, manteniéndose en el anonimato, dirige a los neimoidianos, que están al mando de la Federación. El Jedi Qui-Gon Jinn y su aprendiz Obi-Wan Kenobi convencen a Amidala para que vaya a Coruscant, la capital de la República y sede del Consejo Jedi, y trate de neutralizar esta amenaza. Pero, al intentar esquivar el bloqueo, la nave real resulta averiada, viéndose así obligada la tripulación a aterrizar en el desértico y remoto planeta de Tatooine...",
+        description: "Experience the heroic action and unforgettable adventures of Star Wars: Episode I - The Phantom Menace. See the first fateful steps in the journey of Anakin Skywalker. Stranded on the desert planet Tatooine after rescuing young Queen Amidala from the impending invasion of Naboo, Jedi apprentice Obi-Wan Kenobi and his Jedi Master Qui-Gon Jinn discover nine-year-old Anakin, a young slave unusually strong in the Force. Anakin wins a thrilling Podrace and with it his freedom as he leaves his home to be trained as a Jedi. The heroes return to Naboo where Anakin and the Queen face massive invasion forces while the two Jedi contend with a deadly foe named Darth Maul. Only then do they realize the invasion is merely the first step in a sinister scheme by the re-emergent forces of darkness known as the Sith.",
         actors: "Liam Neeson, Ewan McGregor, Natalie Portman, Jake Lloyd, Samuel L. Jackson, Ian McDiarmid, Ray Park, Anthony Daniels, Kenny Baker, Pernilla August, Hugh Quarshie, Ahmed Best, Andy Secombe, Frank Oz, Terence Stamp, Keira Knightley, Oliver Ford Davies, Ralph Brown, Warwick Davis, Sofia Coppola, Dominic West, Silas Carson"
     },
     {
 
         id:5,
-        title:"Star wars episodio II: El ataque de los clones",
+        title:"Star wars episode II: Attack of the clones",
         image:"https://picfiles.alphacoders.com/124/124924.jpg",
-        description:"Corren tenebrosos tiempos para la República, que continúa envuelta en luchas y sumida en el caos. Un movimiento separatista, formado por centenares de planetas y poderosas alianzas encabezadas por el misterioso conde Dooku, amenaza la galaxia. Ni siquiera los Jedi parecen capaces de conjurar el peligro. Este movimiento provoca el estallido de las guerras clones, que representa el principio del fin de la República. Para allanar el camino, los separatistas intentan asesinar a la senadora Padme Amidala. Para evitar futuros atentados, su seguridad es encomendada a dos caballeros Jedi...",
+        description:"Watch the seeds of Anakin Skywalker's transformation take root in Star Wars: Episode II - Attack of the Clones. Ten years after the invasion of Naboo, the galaxy is on the brink of civil war. Under the leadership of a renegade Jedi named Count Dooku, thousands of solar systems threaten to break away from the Galactic Republic. When an assassination attempt is made on Senator Padmé Amidala, the former Queen of Naboo, twenty-year-old Jedi apprentice Anakin Skywalker is assigned to protect her. In the course of his mission, Anakin discovers his love for Padmé as well as his own darker side. Soon, Anakin, Padmé, and Obi-Wan Kenobi are drawn into the heart of the Separatist movement and the beginning of the Clone Wars.",
         actors:"Hayden Christensen, Ewan McGregor, Natalie Portman, Ian McDiarmid, Samuel L. Jackson, Christopher Lee, Temuera Morrison, Pernilla August, Jimmy Smits, Anthony Daniels, Kenny Baker, Frank Oz, Andy Secombe, Rose Byrne, Oliver Ford Davies, Jack Thompson, Joel Edgerton, Ahmed Best, Silas Carson, Bonnie Piesse, Matt Doran"
     },
     {
         id:6,
-        title:"Star wars episodio III: La venganza de los Sith",
+        title:"Star wars episode III: Revenge of the Sith",
         image:"https://th.bing.com/th/id/R.70c768bbe1fa03ba9bc2c3dc233b5629?rik=VLPB0230p9rBFQ&pid=ImgRaw&r=0",
-        description:"Último capítulo de la trilogía de precuelas de Star Wars, en el que Anakin Skywalker definitivamente se pasa al lado oscuro. En el Episodio III aparece el General Grievous, un ser implacable mitad-alien mitad-robot, el líder del ejército separatista Droid. Los Sith son los amos del lado oscuro de la Fuerza y los enemigos de los Jedi. Fueron prácticamente exterminados por los Jedi hace mil años, pero esta orden del mal sobrevivió en la clandestinidad.",
+        description:"Discover the true power of the dark side in Star Wars: Episode III - Revenge of the Sith. Years after the onset of the Clone Wars, the noble Jedi Knights lead a massive clone army into a galaxy-wide battle against the Separatists. When the sinister Sith unveil a thousand-year-old plot to rule the galaxy, the Republic crumbles and from its ashes rises the evil Galactic Empire. Jedi hero Anakin Skywalker is seduced by the dark side of the Force to become the Emperor’s new apprentice – Darth Vader. The Jedi are decimated, as Obi-Wan Kenobi and Jedi Master Yoda are forced into hiding.",
         actors:"Hayden Christensen, Ewan McGregor, Natalie Portman, Ian McDiarmid, Samuel L. Jackson, Jimmy Smits, Anthony Daniels, Kenny Baker, Frank Oz, Christopher Lee, Peter Mayhew, Silas Carson, Keisha Castle-Hughes, Temuera Morrison, Trisha Noble, Bruce Spence, Ahmed Best, Joel Edgerton, Bonnie Piesse, George Lucas"
     },
     {
         id:7,
-        title: "Star wars episodio VII: El despertar de la fuerza",
+        title: "Star wars episode VII: The force awakens",
         image:"https://th.bing.com/th/id/R.15b623d1e2d618c46b2848273edf7767?rik=Ii2blK0aAks1wg&pid=ImgRaw&r=0",
-        description:"Treinta años después de la victoria de la Alianza Rebelde sobre la segunda Estrella de la Muerte (hechos narrados en el Episodio VI: El retorno del Jedi), la galaxia está todavía en guerra. Una nueva República se ha constituido, pero una siniestra organización, la Primera Orden, ha resurgido de las cenizas del Imperio Galáctico. A los héroes de antaño, que luchan ahora en la Resistencia, se suman nuevos héroes: Poe Dameron, un piloto de caza, Finn, un soldado desertor de la Primera Orden, Rey, una joven chatarrera, y BB-8, un androide rodante. Todos ellos luchan contra las fuerzas del Mal: el Capitán Phasma, de la Primera Orden, y Kylo Ren, un temible y misterioso personaje que empuña un sable de luz roja.",
+        description:"Thirty years since the destruction of the second Death Star, the sinister First Order, commanded by the mysterious Snoke and apprentice Kylo Ren, rise from the ashes of the Empire. The Resistance, led by General Leia Organa, attempts to thwart the First Order's threat, but they're desparate for help. Rey, a desert scavenger, and Finn, an ex-stormtrooper, find themselves joining forces wiiht Han Solo and Chewbacca in a desperate mission to return a BB-unit droid with a map to Luke Skywalker back to the Resistance.",
         actors:"Daisy Ridley, John Boyega, Harrison Ford, Adam Driver, Oscar Isaac, Carrie Fisher, Peter Mayhew, Domhnall Gleeson, Max von Sydow, Gwendoline Christie, Lupita Nyong'o, Andy Serkis, Anthony Daniels, Mark Hamill, Greg Grunberg, Kenny Baker, Simon Pegg, Christina Chong, Miltos Yerolemou, Ken Leung, Thomas Brodie-Sangster, Harriet Walter, Iko Uwais, Yayan Ruhian, Warwick Davis, Jessica Henwick, Daniel Craig, Billie Lourd, Judah Friedlander, Liang Yang, Hannah John-Kamen"
     },
     {
         id:8,
-        title:"Star wars episodio VIII: Los últimos Jedi",
+        title:"Star wars episode VIII: The last Jedi",
         image:"https://th.bing.com/th/id/R.52d6ca082edd062b8841527ee3b23b7e?rik=96KSOooo85i8iA&pid=ImgRaw&r=0",
-        description:"La malvada Primera Orden se ha vuelto más poderosa y tiene contra las cuerdas a la Resistencia, liderada por la General Leia Organa (Carrie Fisher). El piloto Poe Dameron (Oscar Isaac) encabeza una misión para intentar destruir un acorazado de la Primera Orden. Mientras tanto, la joven Rey (Daisy Ridley) tendrá que definir su futuro y su vocación, y el viejo jedi Luke Skywalker (Mark Hamill) revaluar el significado de su vida.",
+        description:"The Resistance is in desperate need of help when they find themselves impossibly pursued by the First Order. While Rey travels to a remote planet called Ahch-To to recruit Luke Skywalker to the Resistance, Finn and Rose, a mechanic, go on their own mission in the hopes of helping the Resistance finally escape the First Order. But everyone finds themselves on the salt-planet of Crait for a last stand.",
         actors:"Daisy Ridley, John Boyega, Adam Driver, Oscar Isaac, Mark Hamill, Carrie Fisher, Kelly Marie Tran, Domhnall Gleeson, Benicio del Toro, Laura Dern, Andy Serkis, Gwendoline Christie, Lupita Nyong'o, Anthony Daniels, Frank Oz, Warwick Davis, Veronica Ngo, Justin Theroux, Billie Lourd, Lily Cole, Navin Chowdhry, Temirlan Blaev, Sara Heller, Josiah Oniha, Joseph Gordon-Levitt, Liang Yang, Michaela Coel, Hermione Corfield"
     },
     {
         id:9,
-        title:"Star wars episodio IX: El ascenso de Skywalker",
+        title:"Star wars episode IX: The rise of Skywalker",
         image:"https://th.bing.com/th/id/OIP.s0RL_a8n9rSK9XzupHaxgwHaLH?pid=ImgDet&rs=1",
-        description:"Un año después de los eventos de Los últimos Jedi, los restos de la Resistencia se enfrentarán una vez más a la Primera Orden, involucrando conflictos del pasado y del presente. Mientras tanto, el antiguo conflicto entre los Jedi y los Sith llegará a su clímax, lo que llevará a la saga de los Skywalker a un final definitivo. Final de la trilogía iniciada con El despertar de la Fuerza.",
+        description:"Lucasfilm and director J.J. Abrams join forces once  more to take viewers on an epic journey to a galaxy far, far away with Star Wars: The Rise of Skywalker, the riveting conclusion of the  landmark Skywalker saga, in which new legends will be born and the final battle for freedom is yet to come.",
         actors:"Daisy Ridley, Adam Driver, John Boyega, Oscar Isaac, Kelly Marie Tran, Joonas Suotamo, Domhnall Gleeson, Ian McDiarmid, Carrie Fisher, Anthony Daniels, Keri Russell, Billie Lourd, Lupita Nyong'o, Billy Dee Williams, Naomi Ackie, Richard E. Grant, Dominic Monaghan, Freddie Prinze Jr., Greg Grunberg, Jimmy Vee, Denis Lawson, Richard Bremmer, Amir El-Masry, Dave Chapman, Harrison Ford, Mark Hamill, Nasser Memarzia, Simon Paisley Day, Brian Herring, Philicia Saunders, Lin-Manuel Miranda, Jodie Comer, Billy Howle, Warwick Davis, Cailey Fleming, Ann Firbank, John Williams, Jacob Fortune-Lloyd"
     }
 ]

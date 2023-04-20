434f46e1bcd6644a9f24a5a1ff3ad5d4038128d6
Nerea <NereaMansillaBarreiro@gmail.com>
2023-04-20 01:35:44 -0300

    Feature:modified responsive carrusel and moviecard

--- starwarss/src/Components/MoviesCard/MoviesCard.jsx
+++ starwarss/src/Components/MoviesCard/MoviesCard.jsx
@@ -22,13 +22,16 @@ if(isLoading) return <Loading2/>
         data?.map((movie)=>(
           <div key={movie.id}>
 
-          <div className='bg-[#0000008e] w-[80vw] h-[90vh] flex flex-col justify-center items-center p-[10px]  md:hidden'>
+          <div className='bg-[#0000008e] w-[80vw] h-[80vh] flex flex-col justify-center items-center p-[10px]  md:hidden'>
             <h1 className='text-white text-[15px] font-[600] sm:text-[20px] sm:mb-[5px]'>{movie.title}</h1>
             <img className='h-[300px] w-[200px] sm:w-[250px] sm:h-[350px]' src={movie.image} alt={movie.title} />
-         
+            <br/>
+          <div className=' h-[30vh] overflow-y-auto' id='scrollbar'>
+
             <h3 className='text-white text-[14px] font-[600] sm:text-[15px]'>{movie.description}</h3>
             <h3 className='text-white text-[12px]  sm:text-[13px]'>Reparto:</h3>
             <h3 className='text-white text-[12px] sm:text-[13px]'>{movie.actors}</h3>
+          </div>
           </div>
 
         <div className='bg-[#0000008e] w-[90vw] h-[90vh] hidden  md:flex md:justify-center md:items-center md:p-[10px] lg:p-[15px]'>
